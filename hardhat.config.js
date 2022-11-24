57a16e6fd964ab8e16c8173962f0d186df5ef4d0
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-24 08:43:31 +0100

    refac: setup for goerli deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,22 @@
+require("hardhat-deploy");
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks:{
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 5,
+      saveDeployments: true,
+    }
+  },
+  namedAccounts:{
+    deployer:{
+      default: 0,
+    }
+  },
   solidity: "0.8.7",
 };
